077d762b85b329051fc3b6c0d9bf2e9da8b4e8ae
Michael Lee "Goliath" Sewell <michael.sewell@senacor.com>
2018-03-08 14:00:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,9 @@ brew bundle
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Install Composer
 #curl -sS https://getcomposer.org/installer | php
 #mv composer.phar /usr/local/bin/composer
